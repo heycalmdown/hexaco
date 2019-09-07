086aaf3388bd15c3ab6b374ee53a1b9f8ca19737
heycalmdown <heyacct@gmail.com>
2019-09-07 15:23:26 +0900

    inline keyboard 형식으로 바꿈

--- src/app.ts
+++ src/app.ts
@@ -2,30 +2,29 @@ import Telegraf, { ContextMessageUpdate } from 'telegraf';
 import * as TT from 'telegram-typings';
 import * as s3 from './s3';
 
-function makeButton(text: string) {
-  return { text };
+function makeButton(text: string, score: number): TT.InlineKeyboardButton {
+  return { text, callback_data: score.toString() };
 }
 
 function makeKeyboard(keyboard: string[]) {
   return {
     reply_markup: {
-      keyboard: [
-        keyboard.map(text => ({ text }))
-      ],
-      one_time_keyboard: true
+      inline_keyboard: [
+        keyboard.map((text, i) => ({ text, callback_data: (i + 1).toString() }))
+      ]
     }
   };
 }
 
-function makeScoreButton(): { reply_markup: TT.ReplyKeyboardMarkup } {
+function makeScoreButton(): { reply_markup: TT.InlineKeyboardMarkup } {
   return {
     reply_markup: {
-      keyboard: [
-        ['전혀 그렇지 않다', '그렇지 않은 편이다'].map(makeButton),
-        ['보통이다', '그런 편이다'].map(makeButton),
-        ['매우 그렇다', '...나중에...'].map(makeButton)
-      ],
-      resize_keyboard: true
+      inline_keyboard: [
+        [makeButton('전혀 그렇지 않다', 1), makeButton('그렇지 않은 편이다', 2)],
+        [makeButton('보통이다', 3), makeButton('그런 편이다', 4)],
+        [makeButton('매우 그렇다', 5), makeButton('...나중에...', 0)],
+        [makeButton('취소', -1)]
+      ]
     }
   };
 }
@@ -34,26 +33,57 @@ async function reply(ctx: ContextMessageUpdate, text: string) {
   await bot.telegram.sendMessage(ctx.message!.from!.id, text);
 }
 
+async function requestNewStart(ctx: ContextMessageUpdate) {
+  return ctx.reply('선택해주세요', makeKeyboard(['자기 보고용', '타인 보고용']))
+}
+
+function question(name: string, progress: s3.Progress): string {
+  const type = progress.type === 1 && '자기 보고용' || '타인 보고용';
+  const text = [
+    `${name}님의 ${type} 테스트입니다.`,
+    '',
+    progress.getNextQuestion()
+  ];
+  return text.join('\n');
+}
+
 async function onStart(ctx: ContextMessageUpdate) {
   const previousProgress = await s3.getPreviousProgress(ctx.from!.id.toString());
-  if (!previousProgress) return ctx.reply('선택해주세요', makeKeyboard(['자기 보고용', '타인 보고용']));
+  if (!previousProgress) return requestNewStart(ctx);
 
-  const type = previousProgress.type === 1 && '자기 보고용' || '타인 보고용';
-  await reply(ctx, '진행하던 내용이 있네요 - ' + type);
-  await reply(ctx, previousProgress.answers.length + '번까지 진행했습니다');
-  await sendQuestion(ctx, previousProgress);
+  const sender = ctx.from!;
+  const name = `${sender.last_name} ${sender.first_name}(${sender.username})`;
+  return ctx.reply(question(name, previousProgress), makeScoreButton());
 }
 
-async function onMy(ctx: ContextMessageUpdate) {
-  const progress = new s3.Progress(ctx.from!.id.toString(), 1, []);
-  await progress.updateProgress();
-  return ctx.reply(progress.getNextQuestion(), makeScoreButton());
+async function xxx(ctx: ContextMessageUpdate, name: string, data: number, progress: s3.Progress | null) {
+  console.log(ctx.callbackQuery);
+  if (!progress) {
+    if (data === 1 || data === 2) {
+      const progress = new s3.Progress(ctx.from!.id.toString(), data, []);
+      await progress.updateProgress();
+      return ctx.editMessageText(question(name, progress), makeScoreButton());
+    } else {
+      return ctx.editMessageText('ㅇㅇ 다 없던 걸로 합니다\n다시 시작하려면 /start 입력하세요\n이것 저것 궁금하면 /help 하세요');
+    }
+  }
+
+  if (1 <= data && data <= 5) {
+    await progress.addAnswer(data);
+    return ctx.editMessageText(question(name, progress), makeScoreButton());
+  } else if (data === 0) {
+    return ctx.editMessageText('ㅇㅇ 나중에 계속...\n다시 시작하려면 /start 입력하세요\n이것 저것 궁금하면 /help 하세요');
+  }
+  await progress.cancelProgress();
+  return ctx.editMessageText('ㅇㅇ 다 없던 걸로 합니다\n다시 시작하려면 /start 입력하세요\n이것 저것 궁금하면 /help 하세요');
 }
 
-async function onTheirs(ctx: ContextMessageUpdate) {
-  const progress = new s3.Progress(ctx.from!.id.toString(), 2, []);
-  await progress.updateProgress();
-  return ctx.reply(progress.getNextQuestion(), makeScoreButton());
+async function onMyCallback(ctx: ContextMessageUpdate) {
+  const data = parseInt(ctx.callbackQuery!.data!, 10);
+  const sender = ctx.callbackQuery!.message!.chat;
+  const name = `${sender.last_name} ${sender.first_name}(${sender.username})`;
+  const progress = await s3.getPreviousProgress(ctx.from!.id.toString());
+  return xxx(ctx, name, data, progress);
 }
 
 async function onCompleted(ctx: ContextMessageUpdate, progress: s3.Progress) {
@@ -99,8 +129,7 @@ function onHelp(ctx: ContextMessageUpdate) {
 const bot = new Telegraf(process.env.BOT_TOKEN!)
 bot.start(onStart);
 bot.help(onHelp);
-bot.hears('자기 보고용', onMy);
-bot.hears('타인 보고용', onTheirs);
+bot.on('callback_query', onMyCallback);
 bot.hears('전혀 그렇지 않다', makeScoreHandler(1));
 bot.hears('그렇지 않은 편이다', makeScoreHandler(2));
 bot.hears('보통이다', makeScoreHandler(3));
