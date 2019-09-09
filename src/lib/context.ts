d78047c90cc73bac557921690cfb30d1058ace89
heycalmdown <heyacct@gmail.com>
2019-09-09 16:02:30 +0900

    handlers, context 리팩토링

---
+++ src/lib/context.ts
@@ -0,0 +1,41 @@
+import { ContextMessageUpdate } from 'telegraf';
+import * as tt from 'telegraf/typings/telegram-types';
+
+export class MyContext {
+  constructor(private ctx: ContextMessageUpdate) {
+  }
+
+  getSenderId() {
+    return this.ctx.from!.id.toString();
+  }
+
+  getSenderName() {
+    const sender = this.ctx.callbackQuery && this.ctx.callbackQuery.from || this.ctx.from;
+    if (!sender) return '아무개씨';
+    return `${sender.first_name} ${sender.last_name}`;
+  }
+
+  async reply(text: string, extra?: tt.ExtraReplyMessage) {
+    try {
+      return await this.ctx.reply(text, extra);
+    } catch (e) {
+      return false;
+    }
+  }
+
+  async answerCbQuery(text?: string) {
+    try {
+      return await this.ctx.answerCbQuery(text);
+    } catch (e) {
+      return false;
+    }
+  }
+
+  async editMessageText(text: string, extra?: tt.ExtraEditMessage) {
+    try {
+      return await this.ctx.editMessageText(text, extra);
+    } catch (e) {
+      return false;
+    }
+  }
+}
